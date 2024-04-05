83f84a797eea9c337e8b04b4e95b454cf8fc61e4
arhtur-clerc <arthur.clerc@laplateforme.io>
2024-04-05 16:18:52 +0200

    install express and setup path for work

---
+++ theBigJob/myapp/public/js/script.js
@@ -0,0 +1,31 @@
+let button = document.querySelector("#button");
+let myForm = document.querySelector("#myForm");
+
+myForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let valueLastName = document.querySelector("#lastname").value;
+  let valueFirstName = document.querySelector("#firstname").value;
+  let valueEmail = document.querySelector("#email").value;
+  let valuePassword = document.querySelector("#password").value;
+
+  let formData = {
+    lastName: valueLastName,
+    firstName: valueFirstName,
+    email: valueEmail,
+    password: valuePassword,
+  };
+
+  let data = JSON.parse(localStorage.getItem("usersData"));
+
+  if (!data) {
+    data = { users: [] };
+  }
+
+  data.users.push(formData);
+
+  localStorage.setItem("usersdata", JSON.stringify(data));
+});
+
+let un = 1;
+
+console.log(un);
